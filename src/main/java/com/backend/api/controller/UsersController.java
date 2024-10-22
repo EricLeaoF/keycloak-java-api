b7bb4dd93a589fae448af9e1a3b75bda8db9ee4e
eric.freitas <eric.freitas@icolabora.com>
2024-10-21 23:20:08 -0300

    configured RestTemplate and listingUsers

--- src/main/java/com/backend/api/controller/UsersController.java
+++ src/main/java/com/backend/api/controller/UsersController.java
@@ -1,19 +1,27 @@
 package com.backend.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.api.service.KeycloakService;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
+
+  @Autowired
+  private KeycloakService keycloakService;
   
   @GetMapping
-  @PreAuthorize("hasRole('USER')")
-  public String list() {
-    return "Listando usuários";
+  @PreAuthorize("hasRole('ADMIN')")
+  public String list(@RequestHeader("Authorization") String authHeader) {
+    String accessToken = authHeader.replace("Bearer ", "");
+    return keycloakService.listUsers(accessToken);
   }
 
   @PostMapping
