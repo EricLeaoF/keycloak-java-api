736ba1a23ebe104f9002dbb43af5d51b335d328a
eric.freitas <eric.freitas@icolabora.com>
2024-11-23 17:10:13 -0300

    new method createUser added

--- src/main/java/com/backend/api/controller/UsersController.java
+++ src/main/java/com/backend/api/controller/UsersController.java
@@ -1,13 +1,21 @@
 package com.backend.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.backend.api.dto.UserCreationDTO;
+import com.backend.api.dto.UserDTO;
 import com.backend.api.service.KeycloakService;
 
 @RestController
@@ -18,16 +26,30 @@ public class UsersController {
   private KeycloakService keycloakService;
   
   @GetMapping
+  @CrossOrigin(origins = "http://frontend")
   @PreAuthorize("hasRole('ADMIN')")
-  public String list(@RequestHeader("Authorization") String authHeader) {
+  public ResponseEntity <List<UserDTO>> list(@RequestHeader("Authorization") String authHeader) {
     String accessToken = authHeader.replace("Bearer ", "");
-    return keycloakService.listUsers(accessToken);
+    List<UserDTO> users = keycloakService.listUsers(accessToken);
+    
+    if (users != null && !users.isEmpty()) {
+      return ResponseEntity.ok(users);
+    } else {
+      return ResponseEntity.noContent().build();
+    }
   }
 
   @PostMapping
   @PreAuthorize("hasRole('ADMIN')")
-  public String create() {
-    return "Cadastrando usuários";
+  public ResponseEntity<?> create(@RequestHeader("Authorization") String authHeader, @RequestBody UserCreationDTO userCreationDTO) {
+    String accessToken = authHeader.replace("Bearer ", "");
+    ResponseEntity<?> response = keycloakService.createUser(accessToken, userCreationDTO);
+
+    if (response.getStatusCode() == HttpStatus.CREATED) {
+      // TODO > Define the password for the user
+    }
+
+    return response;
   }
 
 }
--- src/main/java/com/backend/api/service/KeycloakService.java
+++ src/main/java/com/backend/api/service/KeycloakService.java
@@ -1,14 +1,22 @@
 package com.backend.api.service;
 
+import java.util.List;
+
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import com.backend.api.dto.UserCreationDTO;
+import com.backend.api.dto.UserDTO;
 
 @Service
 public class KeycloakService {
@@ -18,15 +26,39 @@ public class KeycloakService {
 
   private final String keycloakUrl = "http://frontend/auth/admin/realms/firstrealm/users";
 
-  public String listUsers(String accessToken) {
+  public List<UserDTO> listUsers(String accessToken) {
     String uri = UriComponentsBuilder.fromHttpUrl(keycloakUrl).toUriString();
 
     HttpHeaders headers = new HttpHeaders();
     headers.setBearerAuth(accessToken);
 
     HttpEntity<String> entity = new HttpEntity<>(headers);
-    ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+    ResponseEntity<List<UserDTO>> response = restTemplate.exchange(uri, HttpMethod.GET, entity, new ParameterizedTypeReference<List<UserDTO>>() {});
     return response.getBody();
+  }
+
+  public ResponseEntity<String> createUser(String accessToken, UserCreationDTO userCreationDTO) {
+    String uri = UriComponentsBuilder.fromHttpUrl(keycloakUrl).toUriString();
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setBearerAuth(accessToken);
+    headers.setContentType(org.springframework.http.MediaType.APPLICATION_JSON);
+
+    String jsonBody = "{" +
+      "\"firstName\": \"" + userCreationDTO.getFirstName() + "\"," +
+      "\"lastName\": \"" + userCreationDTO.getLastName() + "\"," +
+      "\"username\": \"" + userCreationDTO.getUsername() + "\"," +
+      "\"email\": \"" + userCreationDTO.getEmail() + "\"" +
+    "}";
+
+    HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
+
+    try {
+      ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
+      return new ResponseEntity<>(response.getBody(), response.getStatusCode());
+    } catch (HttpClientErrorException | HttpServerErrorException e) {
+      return new ResponseEntity<>(e.getResponseBodyAsString(), e.getStatusCode());
+    }
   }
   
 }
